254ad5edd5e9582261c0545396832fa8b3f24855
Admin <Admin@DESKTOP-4TDVRVS>
2020-01-19 10:02:42 +0200

    process of grammatical exception

--- src/org/tyaa/javaalgorithmsdemo/JavaAlgorithmsDemo.java
+++ src/org/tyaa/javaalgorithmsdemo/JavaAlgorithmsDemo.java
@@ -21,34 +21,32 @@ public class JavaAlgorithmsDemo {
         //System.out.println("Введите любую цифру от 0 до 9:");
         /*Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();*/
-        /*if(x == 2){
+ /*if(x == 2){
             System.out.println("х == 2");
         }*/
-        /*if(x != 2){
+ /*if(x != 2){
             System.out.println("х != 2");
         }*/
-        /*if(x > 2){
+ /*if(x > 2){
             System.out.println("х > 2");
         }*/
-        /*if(x <= 2){
+ /*if(x <= 2){
             System.out.println("х <= 2");
         }*/
-        
-        /*if (x == 0) {
+
+ /*if (x == 0) {
             System.out.println("x == 0");
         } else {
             System.out.println("x не равен 0");
         }*/
-        
-        /*if (x == 0) {
+ /*if (x == 0) {
             System.out.println("x == 0");
         } else if(x > 5) {
             System.out.println("x > 5");
         } else {
             System.out.println("x не равен 0 и х не болшьше 5");
         }*/
-        
-        /*switch(x){
+ /*switch(x){
             case 0:{
                 System.out.println("x == 0");
                 break;
@@ -61,46 +59,47 @@ public class JavaAlgorithmsDemo {
                 System.out.println("x равен не предусмотренному значению");
             }
         }*/
-        /*Scanner sc = new Scanner(System.in);
+ /*Scanner sc = new Scanner(System.in);
         System.out.println("Введите первое целое число:");
         int x = sc.nextInt();
         System.out.println("Введите второе целое число:");
         int y = sc.nextInt();
         
         System.out.println("x = " + x + "; y = " + y);*/
-        
-        /*if (x > 5 && y > 5) {
+ /*if (x > 5 && y > 5) {
             System.out.println("TRUE");
         } else {
             System.out.println("FALSE");
         }*/
-        
-        /*if (x > 5 || y > 5) {
+ /*if (x > 5 || y > 5) {
             System.out.println("TRUE");
         } else {
             System.out.println("FALSE");
         }*/
-        
         System.out.println("Введите целое положительное число:");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int n10 = n % 10;
-        
-        switch(n10){
-            case 1:{
-                System.out.println("Ворона");
-                break;
-            }
-            case 2:
-            case 3:
-            case 4: {
-                System.out.println("Вороны");
-                break;
-            }
-            default:{
-                System.out.println("Ворон");
+        int n100 = n % 100;
+        if (n100 >= 11 && n100 <= 14) {
+            System.out.println("Ворон");
+        } else {
+            int n10 = n % 10;
+            switch (n10) {
+                case 1: {
+                    System.out.println("Ворона");
+                    break;
+                }
+                case 2:
+                case 3:
+                case 4: {
+                    System.out.println("Вороны");
+                    break;
+                }
+                default: {
+                    System.out.println("Ворон");
+                }
             }
         }
     }
-    
+
 }
